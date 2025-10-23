dfcb43ab6f8f9089e3a8d340853e60ae81d15e5f
Jigchu <jinnshun.js@gmail.com>
2025-10-23 22:28:38 +0800

    Added basedpyright + stylua config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,21 +1,25 @@
 return {
-	'stevearc/conform.nvim',
-	opts = {
-		formatters_by_ft = {
-			python = { "isort", "black" },
-			c = { "clang-format" },
-		},
-		formatters = {
-			clang_format = {
-				command = [[clang-format]],
-				prepend_args = { "-style", [[file:"~/.clang-format"]] },
-			},
-			isort = {
-				command = "isort",
-				args = {
-					"-",
-				},
-			},
-		},
-	},
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      python = { "isort", "black" },
+      c = { "clang-format" },
+      lua = { "stylua" },
+    },
+    formatters = {
+      clang_format = {
+        command = [[clang-format]],
+        prepend_args = { "-style", [[file:"~/.clang-format"]] },
+      },
+      isort = {
+        command = "isort",
+        args = {
+          "-",
+        },
+      },
+      stylua = {
+        command = "stylua",
+      },
+    },
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,35 +1,41 @@
 return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		servers = {
-			lua_ls = {
-				mason = false,
-				cmd = {
-					"lua-language-server"
-				},
-			},
-			ltex_plus = {
-				settings = {
-					ltex = {
-						checkFrequency = "save",
-						language = "en-GB",
-						additionalRules = {
-							languageModel = [[~/models/ngrams/]],
-						},
-					},
-				},
-			},
-			clangd = {
-				mason = false,
-				cmd = {
-					"clangd",
-					"--completion-style=detailed",
-				},
-			},
-		}
-	},
-	dependencies = {
-		"mason-org/mason.nvim",
-		"mason-org/mason-lspconfig.nvim",
-	}
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      lua_ls = {
+        mason = false,
+        cmd = {
+          "lua-language-server",
+        },
+      },
+      ltex_plus = {
+        settings = {
+          ltex = {
+            checkFrequency = "save",
+            language = "en-GB",
+            additionalRules = {
+              languageModel = [[~/models/ngrams/]],
+            },
+          },
+        },
+      },
+      clangd = {
+        mason = false,
+        cmd = {
+          "clangd",
+          "--completion-style=detailed",
+        },
+      },
+      basedpyright = {
+        mason = false,
+        cmd = {
+          "basedpyright",
+        },
+      },
+    },
+  },
+  dependencies = {
+    "mason-org/mason.nvim",
+    "mason-org/mason-lspconfig.nvim",
+  },
 }

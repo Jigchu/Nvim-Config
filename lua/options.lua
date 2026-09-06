d750b10cbec7c341c392297eb53223ab78df0683
Jigchu <justgibusername@gmail.com>
2026-09-06 23:51:13 +0800

    Moved options into dedicated file

---
+++ lua/options.lua
@@ -0,0 +1,26 @@
+vim.g.netrw_banner = 0
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.wrap = false
+vim.opt.smartindent = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.laststatus = 3
+
+vim.opt.undofile = true
+
+vim.opt.isfname:append("@-@")
+vim.opt.scrolloff = 8
+
+vim.opt.signcolumn = "yes"

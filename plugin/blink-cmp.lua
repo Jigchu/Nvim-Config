a0cdf890d4a2cc5e0d7198c996478570a5a9d0a3
Jigchu <justgibusername@gmail.com>
2026-09-07 17:11:05 +0800

    remapped show and hide documentation to C-y

--- plugin/blink-cmp.lua
+++ plugin/blink-cmp.lua
@@ -1,10 +1,13 @@
-vim.pack.add({{
+vim.pack.add({ {
   src = "https://github.com/saghen/blink.cmp",
   version = vim.version.range("1.x"),
-}})
+} })
 
 require("blink.cmp").setup({
-  keymap = { preset = "super-tab" },
+  keymap = {
+    preset = "super-tab",
+    ["<C-y>"] = { "show_documentation", "hide_documentation" },
+  },
   sources = {
     default = { "lsp", "path", "snippets", "buffer" },
   },
